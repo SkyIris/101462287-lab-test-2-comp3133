b76f1ca2d92020924c20c5ee39337fd3407bda33
SkyIris <zhongakeen@gmail.com>
2025-04-03 17:58:56 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '101462287-lab-test2-comp3133';
+}
